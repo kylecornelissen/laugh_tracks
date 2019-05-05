d806e3cd81240f9d3b0dbaa9b24d047b6a3f7489
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-05-05 15:04:21 -0600

    Add create, new, and params for comedian controller

--- app/controllers/comedians_controller.rb
+++ app/controllers/comedians_controller.rb
@@ -6,4 +6,23 @@ class ComediansController < ApplicationController
       @comedians = Comedian.all
     end
   end
+
+  def new
+    @comedian = Comedian.new
+  end
+
+  def create
+    comedian = Comedian.new(comedian_params)
+    if comedian.save
+      redirect_to "/comedians"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def comedian_params
+    params.require(:comedian).permit(:name, :age, :city, :img_url)
+  end
 end
